66d0c76f005c15641fe745ad3a927ba3ef592214
OstapHetman <prickly4528@gmail.com>
2017-12-30 11:40:55 +0200

    correct some problem with timer and check text in headers

--- src/js/countdown.js
+++ src/js/countdown.js
@@ -1,4 +1,5 @@
 let countDownDate = new Date("Jan 7, 2018 23:59:25").getTime();
+let countDownDate2 = new Date("Jan 7, 2018 23:59:25").getTime();
 
 // Update the count down every 1 second
 let x = setInterval(function() {
@@ -16,13 +17,31 @@ let x = setInterval(function() {
     document.getElementsByClassName("hours")[0].innerHTML = hours;
     document.getElementsByClassName("minutes")[0].innerHTML = minutes;
     document.getElementsByClassName("seconds")[0].innerHTML = seconds;
-    document.getElementsByClassName("days")[1].innerHTML = '0' + days;
-    document.getElementsByClassName("hours")[1].innerHTML = hours;
-    document.getElementsByClassName("minutes")[1].innerHTML = minutes;
-    document.getElementsByClassName("seconds")[1].innerHTML = seconds;
     // If the count down is finished, write some text 
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
+
+let y = setInterval(function() {
+    // Get todays date and time
+    let now2 = new Date().getTime();
+    // Find the distance between now an the count down date
+    let distance2 = countDownDate2 - now2;
+    // Time calculations for days, hours, minutes and seconds
+    let days2 = Math.floor(distance2 / (1000 * 60 * 60 * 24));
+    let hours2 = Math.floor((distance2 % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes2 = Math.floor((distance2 % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds2 = Math.floor((distance2 % (1000 * 60)) / 1000);
+    // Display the result in the element with id="demo"
+
+    document.getElementById("days2").innerHTML = '0' + days2;
+    document.getElementById("hours").innerHTML = hours2;
+    document.getElementById("minutes").innerHTML = minutes2;
+    document.getElementById("seconds").innerHTML = seconds2;
+    // If the count down is finished, write some text 
+    if (distance < 0) {
+        clearInterval(y);
     }
 }, 1000);
